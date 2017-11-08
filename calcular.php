89f3b8a925027bb01fd97da43d983ffa6548f34d
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2017-11-08 14:22:53 -0200

    Add files via upload

---
+++ calcular.php
@@ -0,0 +1,35 @@
+<?php
+	require_once 'classes/Calculadora.php';
+
+	$numero1 = $_POST['numero1'];
+	$numero2 = $_POST['numero2'];
+	$operacao = $_POST['operacao'];
+
+	$calculadora = new Calculadora();
+
+	//Setar Valores	
+	$calculadora->setNumero1( $numero1);
+	$calculadora->setNumero2( $numero2);
+
+	if($operacao == "somar")
+	{
+		$calculadora->somar();		
+	}
+	if($operacao == "subtrair")
+	{
+		$calculadora->subtrair();		
+	}
+	if($operacao == "multiplicar")
+	{
+		$calculadora->multiplicar();		
+	}
+	if($operacao == "dividir")
+	{
+		$calculadora->dividir();		
+	}
+
+	echo $calculadora->getTotal();
+
+
+
+?>
